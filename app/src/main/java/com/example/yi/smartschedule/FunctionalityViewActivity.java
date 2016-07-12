d706158d6bdcd4517fdcbd05fe861cb313516207
jackphillips7764 <jackiscool20@gmail.com>
2016-07-12 16:48:08 -0400

    implemented wifi toggle

--- app/src/main/java/com/example/yi/smartschedule/FunctionalityViewActivity.java
+++ app/src/main/java/com/example/yi/smartschedule/FunctionalityViewActivity.java
@@ -3,9 +3,12 @@ package com.example.yi.smartschedule;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +16,15 @@ import android.provider.Settings.System;
 
 import com.example.yi.smartschedule.lib.Util;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 public class FunctionalityViewActivity extends AppCompatActivity implements View.OnClickListener {
     AudioManager audio;
+    private boolean wifiInabled = true;
     private ContentResolver cResolver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,9 @@ public class FunctionalityViewActivity extends AppCompatActivity implements View
 
         Button silence = (Button) findViewById(R.id.silent);
         silence.setOnClickListener(this);
+
+        Button wifi = (Button) findViewById(R.id.wifi);
+        wifi.setOnClickListener(this);
 
         Button brightness = (Button) findViewById(R.id.brightness);
         brightness.setOnClickListener(this);
@@ -50,6 +61,9 @@ public class FunctionalityViewActivity extends AppCompatActivity implements View
                 }catch (Settings.SettingNotFoundException e){
                     Util.d("" + e.toString());
                 }
+                break;
+            case R.id.wifi:
+                toggleWifi();
                 break;
 
         }
@@ -77,10 +91,31 @@ public class FunctionalityViewActivity extends AppCompatActivity implements View
         Util.d("Brightness set to: " + brightness);
         //gets current brightness:
     }
+
+    public void toggleWifi(){
+        wifiInabled = !wifiInabled;
+        WifiManager wifiManager = (WifiManager)this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiManager.setWifiEnabled(wifiInabled);
+        Util.d("wifi set to: " + wifiInabled);
+
+    }
     //this is a secure setting so it is read only
-    public void turnOnAirplaneMode(){
-        boolean isEnabled = Settings.Global.getInt(getContentResolver(),Settings.Global.AIRPLANE_MODE_ON, 0) == 1;
-        // toggle airplane mode
-        Settings.Global.putInt(getContentResolver(),Settings.Global.AIRPLANE_MODE_ON, isEnabled ? 0 : 1);
+    public void toggleOnAirplaneMode() throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+
+        //need root for this
+        try{
+            TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            Method setMobileDataEnabledMethod = telephonyManager.getClass().getDeclaredMethod("setDataEnabled", boolean.class);
+
+            if(null!= setMobileDataEnabledMethod){
+                setMobileDataEnabledMethod.invoke(telephonyManager, false);
+                Util.d("Almost done");
+            }
+        }catch (Exception e){
+            Util.d(e.toString());
+        }
+
+        Util.d("Almost airplane mode ");
+
     }
 }
