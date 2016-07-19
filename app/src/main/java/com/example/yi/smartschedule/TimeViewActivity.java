993e94c712ca2d8babcc0089209e53fc1a4ae294
awesomeyi <awesomeyi@gmail.com>
2016-07-19 15:52:36 -0400

    Implemented synchronized scrolling

--- app/src/main/java/com/example/yi/smartschedule/TimeViewActivity.java
+++ app/src/main/java/com/example/yi/smartschedule/TimeViewActivity.java
@@ -17,6 +17,9 @@ import com.example.yi.smartschedule.lib.Time;
 import com.example.yi.smartschedule.lib.TimeLineAdapter;
 import com.example.yi.smartschedule.lib.Util;
 
+import aligningrecyclerview.AligningRecyclerView;
+import aligningrecyclerview.AlignmentManager;
+
 public class TimeViewActivity extends AppCompatActivity {
     private LinearLayout event_list;
     public static final int L_BLOCK_HEIGHT = 70;
@@ -26,14 +29,14 @@ public class TimeViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_view);
 
-        RecyclerView time_line_list = (RecyclerView) findViewById(R.id.time_line_list);
-        RecyclerView main_event_list = (RecyclerView) findViewById(R.id.main_event_list);
+        AligningRecyclerView time_line_list = (AligningRecyclerView) findViewById(R.id.time_line_list);
+        AligningRecyclerView main_event_list = (AligningRecyclerView) findViewById(R.id.main_event_list);
 
         String title = "Meet Ryan";
         String description = "Meet Ryan near the corner of 5th Ave.\nHe's going to be selling you drugs so bring money.";
 
-        Time startTimes[] = { new Time(1, 00), new Time(2, 00), new Time(4, 00)};
-        int minutes[] = {60, 30, 120};
+        Time startTimes[] = { new Time(1, 00), new Time(2, 00), new Time(4, 00), new Time(5, 30), new Time(6, 00), new Time(10, 00)};
+        int minutes[] = {30, 40, 60, 20, 80, 30};
         EventData events[] = new EventData[minutes.length];
 
         for(int i = 0; i < minutes.length; ++i) {
@@ -44,11 +47,16 @@ public class TimeViewActivity extends AppCompatActivity {
         TimeLineAdapter timeLineAdapter = new TimeLineAdapter(eventStore);
         EventAdapter eventAdapter = new EventAdapter(eventStore);
 
+        final @AligningRecyclerView.AlignOrientation int orientation;
+        orientation = AligningRecyclerView.ALIGN_ORIENTATION_VERTICAL;
+
         time_line_list.setAdapter(timeLineAdapter);
-        time_line_list.setLayoutManager(new LinearLayoutManager(this));
+        time_line_list.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
         main_event_list.setAdapter(eventAdapter);
-        main_event_list.setLayoutManager(new LinearLayoutManager(this));
+        main_event_list.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        AlignmentManager.join(orientation, time_line_list, main_event_list);
 
         Util.d("" + eventStore.startEndInInterval(new Time(7, 01), new Time(7, 59)));
     }
